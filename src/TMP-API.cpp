21ca67085e73976ea927e0f63d084105549c271e
Choped7626 <mario.l.a3094@gmail.com>
2026-03-16 18:43:30 +0000

    TMP_RobotServer initial implementation with webserver

--- src/TMP-API.cpp
+++ src/TMP-API.cpp
@@ -1,2 +1,26 @@
 #include "TMP-API.h"
 
+TMP_RobotServer::TMP_RobotServer(int port) : _server(port) {}
+
+void TMP_RobotServer::begin(const char *ssid, const char *passwd) {
+
+  WiFi.softAP(ssid, passwd);
+
+  // routes config using lambdas to access class methods
+  _server.on("/health", HTTP_GET, [this]() { this->handleHealth(); });
+  _server.onNotFound([this]() { this->handleNotFound(); });
+
+  _server.begin();
+}
+
+void TMP_RobotServer::update() { _server.handleClient(); }
+
+void TMP_RobotServer::handleHealth() {
+  _server.send(200, "application/json", "{\"status\":\"ok\"}");
+  Serial.print("Status ok\n");
+}
+
+void TMP_RobotServer::handleNotFound() {
+  _server.send(404, "application/json", "{\"error\":\"not found\"}");
+  Serial.print("Ruta desconocida\n");
+}
--- src/TMP-API.h
+++ src/TMP-API.h
@@ -1,5 +1,28 @@
-#ifdef TMP_API_H
+#ifndef TMP_API_H
+#define TMP_API_H
 
 #include <Arduino.h>
+#include <WebServer.h>
+#include <WiFi.h>
+
+class TMP_RobotServer {
+public:
+  // Constructor: recibe el puerto del servidor
+  TMP_RobotServer(int port = 80);
+
+  // Configura el WiFi y los endpoints
+  void begin(const char *ssid = "ESP32-Hotspot",
+             const char *password = "1234567890");
+
+  // llamar en el loop() del main
+  void update();
+
+private:
+  WebServer _server;
+
+  // Manejadores de rutas (deben ser internos)
+  void handleHealth();
+  void handleNotFound();
+};
 
 #endif // TMP_API_H
