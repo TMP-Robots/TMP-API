023be1bb4a8e8d3cc8055ab1154f2eca0fe8fe0e
alexborrazasm <alexborrazasm@gmail.com>
2026-03-22 17:01:56 +0100

    Add initial example sketch for TMP_RobotServer with WiFi setup and basic loop

---
+++ examples/simple/simple.ino
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <TMP-API.h>
+
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+TMP_RobotServer robotServer(80);
+
+void setup()
+{
+  #ifdef DEBUG_MODE
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    delay(4000); // Wait for Serial to initialize
+  #endif
+  
+  /* Change the last parameter to true if you want the ESP32 to create its own
+   * Access Point instead of connecting to an existing WiFi network */
+  robotServer.begin(ssid, password, false); 
+}
+
+void loop() 
+{
+  robotServer.update();
+}
